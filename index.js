4fcdf22e51d03644fd150a5fbd3d6586e73c6cfd
pranav s <dev.pranabsk@gmail.com>
2023-01-03 13:07:45 +0530

    Create POST api, install postman, read post data, use body parser

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-//npm i express@4.18.2
 //create server
 const express = require("express");
 var cors = require("cors");
+const bodyParser = require("body-parser");
 
 //return express
 const app = express();
 app.use(cors());
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.send("Server Running!");
@@ -18,8 +19,13 @@ app.get("/users/:id", (req, res) => {
   //console.log(req.query.sort);
   const userId = req.params.id;
   const name = users[userId];
-   res.send({userId, name});
-  });
+  res.send({ userId, name });
+});
+
+//Post
+app.post("/api/addUsers", (req, res) => {
+  console.log(req.body);
+});
 
 const port = 5005;
 
@@ -27,8 +33,3 @@ const port = 5005;
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
-
-//node index //run server
-//localhost:5005
-//nodemon index //run server(auto reload)
-//localhost:5005/a
