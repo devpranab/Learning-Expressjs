c0997b91def1ae0200c598192d2f930bd762ea81
pranav s <dev.pranabsk@gmail.com>
2023-01-03 12:33:58 +0530

    Dynamic api, api parameter, access params, access query

--- index.js
+++ index.js
@@ -8,12 +8,14 @@ app.get("/", (req, res) => {
   res.send("Server Running!");
 });
 
-app.get("/me", (req, res) => {
-    const me = {
-        Name: "Pranab Sarkar",
-        Prof: "Pragrammer"
-    }
-    res.send(me);
+const users = ["Pranab", "Araj", "Miraj", "Avik"];
+
+app.get("/users/:id", (req, res) => {
+  //console.log(req.params.id);
+  //console.log(req.query.sort);
+  const userId = req.params.id;
+  const name = users[userId];
+   res.send({userId, name});
   });
 
 const port = 5005;
